089ecae1f6ae4e53f18e11018d98e86746a0d87e
nemone09 <sunyounglee.biz@gmail.com>
2025-07-25 18:11:44 +0900

    최종 파일

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,13 @@
+// 작업 1
 public class Calculator {
     public static void main(String[] args) {
+        // 논리 구조 1
         System.out.println("1+1 = 2");
+        System.out.println("1*2 = 2");
+        System.out.println("8/2 = 2");
+        System.out.println("5%3 = 2");
+        System.out.println("6+1 = 7");
+        System.out.println("99+1 = 100");
+        // 작업 완료 1
     }
 }
